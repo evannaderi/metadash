910855673572535c6d4761c7ab0f6472e78021d7
Evan Naderi <evan.naderi@gmail.com>
2023-09-22 15:00:49 -0400

    added donut

--- dash_app/layout.py
+++ dash_app/layout.py
@@ -2,6 +2,44 @@ import dash_core_components as dcc
 import dash_html_components as html
 from dash_app import app
 from dash.dependencies import Input, Output
+import plotly.express as px
+import pandas as pd
+
+data = {
+    'Name': ['Alice', 'Bob'],
+    'Score': [90, 85]
+}
+
+df = pd.DataFrame(data)
+
+fig = px.pie(df, names='Name', values='Score', hole=0.3)
+fig.update_traces(textinfo='none')
+fig.update_layout(
+    title_text='',
+    showlegend=False,
+    # width=,  # Set the width to 110 pixels
+    # height=130  # Set the height to 130 pixels
+)
+
+grid_cells = []
+
+for i in range(8):
+    if i == 5:
+        cell = html.Div("hello", id=f"cell-{i}", className='grid-item-10x8', style={'color': 'red'})
+    if i == 6:
+        cell = html.Div([
+            dcc.Graph(
+                id='example-graph',
+                figure=fig
+            )
+        ], id=f"cell-{i}", className='grid-item')
+
+    else:
+        cell = html.Div("", id=f"cell-{i}", className='grid-item-10x8') 
+
+    grid_cells.append(cell)    
+
+           
 
 app.layout = html.Div([
     html.H1(
@@ -19,12 +57,7 @@ app.layout = html.Div([
             ),
             
             # Right side 10x8 grid
-            html.Div(
-                [
-                    html.Div("", className='grid-item-10x8') for _ in range(80)  # 10x8 = 80
-                ],
-                className='grid-10x8'
-            )
+            html.Div(grid_cells, className='grid-10x8')
         ],
         className='main-container'
     )
