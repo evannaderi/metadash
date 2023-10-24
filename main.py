bbc8b9b7622ca85a37a054e67608805920bfc10c
Evan Naderi <evan.naderi@gmail.com>
2023-10-24 10:56:17 -0400

    changed port to 5001

--- main.py
+++ main.py
@@ -2,4 +2,4 @@ from flask_app import server
 from dash_app import app  # This import is needed even if it's unused; it initializes the Dash app
 
 if __name__ == '__main__':
-    server.run(debug=True)
+    server.run(debug=True, port=5001)
