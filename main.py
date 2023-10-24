765cd94356d3ac8c96cedf772c434731f94b83ef
Evan Naderi <evan.naderi@gmail.com>
2023-10-24 11:01:38 -0400

    added comment

--- main.py
+++ main.py
@@ -3,3 +3,8 @@ from dash_app import app  # This import is needed even if it's unused; it initia
 
 if __name__ == '__main__':
     server.run(debug=True, port=5001)
+
+    # To user: run this file to start the server
+    # After running, go to http://127.0.0.1:5001/dashboard in your browser
+
+    # The github repo link is https://github.com/evannaderi/metadash
