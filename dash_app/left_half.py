00869d77a1da84bc483b043c0a31545a40cfe1f5
Evan Naderi <evan.naderi@gmail.com>
2023-10-26 11:14:12 -0400

    tried

--- dash_app/left_half.py
+++ dash_app/left_half.py
@@ -1,20 +1,50 @@
 import dash_core_components as dcc
 import dash_html_components as html
+import plotly.express as px
+from dash import html
+
+def create_donut_chart(num, color):
+        fig = px.pie(values=[num, 1 - num], hole=0.5)
+        fig.update_traces(textinfo='none', marker=dict(colors=[color, 'grey']))
+        fig.update_layout(
+            showlegend=False,
+            margin=dict(l=0, r=0, t=0, b=0),
+            paper_bgcolor='black',
+            annotations=[dict(
+                    text=str(round(num * 100)) + '%', 
+                    x=0.5, 
+                    y=0.5, 
+                    font_size=20, 
+                    showarrow=False,
+                    font=dict(
+                        color='white'
+                    )
+            )]
+        )
+        return fig
 
 def get_left_half():
     return html.Div([
         # 3x3 Grid
         html.Div([
             # First Row: Graphs
-            html.Div([dcc.Graph(id='engaged-graph', figure={'data': [], 'layout': {}})], style={'width': '33.33%', 'display': 'inline-block'}),
-            html.Div([dcc.Graph(id='confused-graph', figure={'data': [], 'layout': {}})], style={'width': '33.33%', 'display': 'inline-block'}),
-            html.Div([dcc.Graph(id='frustrated-graph', figure={'data': [], 'layout': {}})], style={'width': '33.33%', 'display': 'inline-block'}),
+            html.Div([dcc.Graph(id='engaged-graph', figure=create_donut_chart(.5, 'green'))], style={'width': '33.33%', 'display': 'inline-block'}),
+            html.Div([dcc.Graph(id='confused-graph', figure=create_donut_chart(.5, 'yellow'))], style={'width': '33.33%', 'display': 'inline-block'}),
+            html.Div([dcc.Graph(id='frustrated-graph', figure=create_donut_chart(.5, 'orange'))], style={'width': '33.33%', 'display': 'inline-block'}),
             
             # Second Row: Buttons
-            html.Div([html.Button('Emotion Triggers', id='emotion-button')], style={'width': '33.33%', 'display': 'inline-block'}),
-            html.Div([html.Button('Individual performance', id='performance-button')], style={'width': '33.33%', 'display': 'inline-block'}),
-            html.Div([html.Button('Feedback on phase', id='feedback-button')], style={'width': '33.33%', 'display': 'inline-block'}),
-            
+            html.Div([
+                html.A([
+                    html.Img(src='icons/sad-face.png', style={'width': '100%', 'height': '100%'})
+                ], id='emotion-button', href='https://www.google.com', style={'width': '33.33%', 'display': 'inline-block'}),
+                html.A([
+                    html.Img(src='./icons/meter.png', style={'width': '100%', 'height': 'auto'})
+                ], id='performance-button', href='https://www.google.com', style={'width': '33.33%', 'display': 'inline-block'}),
+                html.A([
+                    html.Img(src='./icons/message.png', style={'width': '100%', 'height': 'auto'})
+                ], id='feedback-button', href='https://www.google.com', style={'width': '33.33%', 'display': 'inline-block'}),
+                # ... (continue for other buttons)
+            ]),
             # Third Row: Buttons
             html.Div([html.Button('Private message', id='private-message-button')], style={'width': '33.33%', 'display': 'inline-block'}),
             html.Div([html.Button('Individual Time on Task', id='time-on-task-button')], style={'width': '33.33%', 'display': 'inline-block'}),
