f4636c11e894e234b49f8ce4b097ef1c3fd982ec
Evan Naderi <evan.naderi@gmail.com>
2023-10-27 13:13:29 -0400

    made opaque work

--- dash_app/layout.py
+++ dash_app/layout.py
@@ -9,6 +9,13 @@ import os
 import sys
 from .left_half import get_left_half
 
+GREEN = (0, 255, 0)
+RED = (255, 0, 0)
+YELLOW = (255, 255, 0)
+ORANGE = (255, 165, 0)
+LOWER_OPACITY = 0.3
+HIGHER_OPACITY = 1
+
 class Student:
     def __init__(self, name, filename="data/student1.csv"):
         self.name = name
@@ -31,16 +38,18 @@ class Student:
             "boredom": 0,
         }
         self.current_row_df = 1
+        self.current_max_emotion = "engagement" # default value
     
     def increment_count(self, emotion):
         self.max_emotion_counts[emotion] += 1
         self.total_row_counts += 1
+        self.current_max_emotion = emotion
     
     def update_rates(self):
         for emotion in self.max_emotion_counts:
             self.rates[emotion] = self.max_emotion_counts[emotion] / self.total_row_counts
 
-    def create_figures(self):
+    def create_figures(self, max_emotion_code):
         self.figures["engagement"] = self.create_engaged_chart(self.rates["engagement"])
         self.figures["confusion"] = self.create_confused_chart(self.rates["confusion"])
         self.figures["frustration"] = self.create_frustrated_chart(self.rates["frustration"])
@@ -49,23 +58,43 @@ class Student:
     
     def create_engaged_chart(self, num):
         #print(f"Creating donut chart with num: {num}, 1-num: {1-num}") # debug
-        return self.create_donut_chart(num, "green")
+        if self.current_max_emotion == "engagement":
+            opacity = HIGHER_OPACITY
+        else:
+            opacity = LOWER_OPACITY
+        return self.create_donut_chart(num, GREEN, opacity)
     
     def create_confused_chart(self, num):
+        if self.current_max_emotion == "confusion":
+            opacity = HIGHER_OPACITY
+        else:
+            opacity = LOWER_OPACITY
         #print(f"Creating donut chart with num: {num}, 1-num: {1-num}") # debug
-        return self.create_donut_chart(num, "yellow")
+        return self.create_donut_chart(num, YELLOW, opacity)
     
     def create_frustrated_chart(self, num):
+        if self.current_max_emotion == "frustration":
+            opacity = HIGHER_OPACITY
+        else:
+            opacity = LOWER_OPACITY
         #print(f"Creating donut chart with num: {num}, 1-num: {1-num}") # debug
-        return self.create_donut_chart(num, "orange")
+        return self.create_donut_chart(num, ORANGE, opacity)
     
     def create_bored_chart(self, num):
         #print(f"Creating donut chart with num: {num}, 1-num: {1-num}") # debug
-        return self.create_donut_chart(num, "red")
+        if self.current_max_emotion == "boredom":
+            opacity = HIGHER_OPACITY
+        else:
+            opacity = LOWER_OPACITY
+        return self.create_donut_chart(num, RED, opacity)
 
-    def create_donut_chart(self, num, color):
+    def create_donut_chart(self, num, color, opacity):
+        # Adjusting the color's alpha value for opacity
+        opaque_color = f"rgba({color[0]},{color[1]},{color[2]},{opacity})"
+        grey_opacity = f"rgba(128, 128, 128, {opacity})"
+
         fig = px.pie(values=[num, 1 - num], hole=0.5)
-        fig.update_traces(textinfo='none', marker=dict(colors=[color, 'grey']))
+        fig.update_traces(textinfo='none', marker=dict(colors=[opaque_color, grey_opacity]))
         fig.update_layout(
             showlegend=False,
             margin=dict(l=0, r=0, t=0, b=0),
@@ -93,14 +122,16 @@ students.append(Student("Student5", "data/DataNumbers.csv"))
 
 current_row = 1
 
+# The left side is what is in the excel file for each
 emotions_codes = {
-    1: "engagement",
-    2: "confusion",
-    3: "frustration",
-    4: "boredom",
+    1 : "engagement",
+    2 : "confusion",
+    3 : "frustration",
+    4 : "boredom",
 }
 
 app.layout = html.Div([
+    html.H1("Teacher Dashboard", style={'text-align': 'center'}),
     get_left_half(),
     html.Div([
         html.Button("Add Student", id="add_row_btn", n_clicks=0),
@@ -139,7 +170,7 @@ def update_charts(add_row_clicks, n_intervals, name):
             row = student.df.iloc[student.current_row_df]
             student.increment_count(emotions_codes[row["MaxEmotionCode"]])
             student.update_rates()
-            student.create_figures()
+            student.create_figures(row["MaxEmotionCode"])
             student.current_row_df += 1
         
         for fig in student.figures:
